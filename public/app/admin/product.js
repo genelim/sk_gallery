12c22f634fdf1fcc8c40b8e7c1e784104dab54f9
Gene Lim <Gene Lim>
2016-04-24 17:14:10 +0800

    Get categories on products

--- public/app/admin/product.js
+++ public/app/admin/product.js
@@ -2,16 +2,24 @@ angular
     .module('app')
     .controller('AdminProductController', AdminProductController);
 
-AdminProductController.$inject = [];
+AdminProductController.$inject = ['Product_Main_Category'];
 
-function AdminProductController(){
+function AdminProductController(Product_Main_Category){
     var vm = this;
     $('.modal-trigger').leanModal();
-
-    var vm = this;
     vm.add_product = add_product;
+    vm.get_category = get_category;
+    vm.category = null;
+    vm.new_product = null;
     
     function add_product(){
-            
+        console.log(vm.new_product)    
+    }
+    
+    get_category()
+    function get_category(){
+        Product_Main_Category.query(function(result){
+            vm.category = result.response
+        })
     }
 }
